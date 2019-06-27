a5060e795f922ef8b846c7ff9efcf3305a3a2ffe
unknown <patil>
2019-06-27 08:32:36 -0300

    Ashutosh

---
+++ app/src/main/java/com/example/abcrentals/signin_app.java
@@ -0,0 +1,13 @@
+package com.example.abcrentals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signin_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_app);
+    }
+}
